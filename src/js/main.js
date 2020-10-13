9390ff91e4a4174d74eea31abb015880c0f87a22
shpetimsedolli <shpeti101@gmail.com>
2020-10-13 11:25:00 +0200

    Websiste with responsive , javascript

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+(function ($, window, document) {
+    "use strict";
+    $(() => {
+        //##Variables
+        //End Variablesn
+        document.querySelector(".hamburger").addEventListener("click", function (e) {
+          document.querySelector(".nav_wrapper").classList.toggle("is--toggled");
+        });
+      $(document).scroll(function () {
+        if ($(document).scrollTop() > $(".first_banner").outerHeight()) {
+          $(".main_header").addClass("is--sticky");
+        } else {
+          $(".main_header").removeClass("is--sticky");
+        }
+      });
+    });
+  }
+  )(window.jQuery, window, document);
